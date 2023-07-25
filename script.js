e70693c5453d02253ba1e2555a4dad235bbb9f02
Furqan Nazakat <134845387+Furqan0507@users.noreply.github.com>
2023-07-25 23:35:40 +0100

    added visit counter

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-<script>
-document.getElementById("demo").innerHTML = "Welcome to my Portfolio. Work in Progress.";
-</script>
+// Function to fetch the visit count from GitHub's API and update the counter
+function updateVisitCounter() {
+    // Fetch the visit count from GitHub's API
+    fetch('https://api.github.com/repos/furqan0507/furqan0507.github.io')
+        .then(response => response.json())
+        .then(data => {
+            const visitCount = data.watchers_count;
+            document.getElementById('counter').textContent = visitCount;
+        })
+        .catch(error => {
+            console.error('Error fetching visit count:', error);
+        });
+}
+
+// Call the function to update the counter when the page loads
+updateVisitCounter();
